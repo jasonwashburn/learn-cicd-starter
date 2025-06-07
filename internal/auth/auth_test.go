a2fe199d52ca59a510d3e4c2d368589a4f6c5d9c
Jason Washburn <jason.washburn@gmail.com>
2025-06-07 06:27:59 -0500

    ci: Update workflow to run tests and add auth tests

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,58 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	tests := []struct {
+		name    string
+		headers http.Header
+		want    string
+		wantErr bool
+	}{
+		{
+			name:    "no auth header",
+			headers: http.Header{},
+			want:    "",
+			wantErr: true,
+		},
+		{
+			name: "no api key",
+			headers: http.Header{
+				"Authorization": []string{"Bearer 1234567890"},
+			},
+			want:    "",
+			wantErr: true,
+		},
+		{
+			name: "valid api key",
+			headers: http.Header{
+				"Authorization": []string{"ApiKey 1234567890"},
+			},
+			want:    "1234567890",
+			wantErr: false,
+		},
+		{
+			name: "fail",
+			headers: http.Header{
+				"Authorization": []string{"Intentional failure"},
+			},
+			want:    "Intentional failure",
+			wantErr: false,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got, err := GetAPIKey(test.headers)
+			if (err != nil) != test.wantErr {
+				t.Errorf("GetAPIKey() error = %v, wantErr %v", err, test.wantErr)
+			}
+			if got != test.want {
+				t.Errorf("GetAPIKey() = %v, want %v", got, test.want)
+			}
+		})
+	}
+}
